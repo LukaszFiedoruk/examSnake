7b72624741a35453525475aa2d968ede8cb4f75b
LukaszFiedoruk <lukas.fiedoruk@gmail.com>
2023-01-18 15:37:25 +0100

    NGX Snake

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  currentPage = 'home';
   title = 'examSnake';
+
+  changePage(page: string) {
+    this.currentPage = page;
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,15 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import {NgxSnakeModule} from 'ngx-snake';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NgxSnakeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
