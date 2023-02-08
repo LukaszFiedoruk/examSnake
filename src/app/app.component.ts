cf67b35cca2f1284c2acd91ea99397278db2e861
LukaszFiedoruk <lukas.fiedoruk@gmail.com>
2023-02-08 19:29:04 +0100

    All works- dosen't reset points and without val...

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,8 +8,8 @@ import { Component } from '@angular/core';
 export class AppComponent {
   playerName: string = '';
   playerEmail: string = '';
-  welcomePage = true;
-  showGame = false;
+  welcomePage = false;
+  showGame = true;
   points = 0;
   startTime: number = 0;
   elapsedTime: number = 0;
